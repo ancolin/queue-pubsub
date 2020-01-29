96a8341915d23e83478cb14b1be2e865e59d1572
ancolin <anco.donuts@gmail.com>
2020-01-29 18:04:59 +0900

    add push queue

--- publisher/app.py
+++ publisher/app.py
@@ -1,13 +1,28 @@
 # coding: utf-8
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request, abort
+import redis
+import json
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def api_sample():
-    result = {"code": "001", "name": "apple"}
-    return jsonify(ResultSet=result)
+@app.route('/', methods=['POST'])
+def orderQueue():
+    result = "NG"
+    message = ""
+    try:
+        r = redis.Redis(host='127.0.0.1', port=6379, db=0)
+        r.lpush('orderQueue', json.dumps(request.json))
+        result = "OK"
+    except Exception as e:
+        message = e
+
+    if result == "OK":
+        response = {"result": result}
+    else:
+        response = {"result": result, "errors": message}
+
+    return jsonify(ResultSet=response)
 
 
 if __name__ == '__main__':
