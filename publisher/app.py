57c94d8edf074b861ff4fbee8d47cca939ffc6ba
ancolin <anco.donuts@gmail.com>
2020-01-29 18:23:20 +0900

    fix queueing

--- publisher/app.py
+++ publisher/app.py
@@ -2,6 +2,8 @@
 from flask import Flask, jsonify, request, abort
 import redis
 import json
+import datetime
+import uuid
 
 app = Flask(__name__)
 
@@ -14,11 +16,18 @@ def orderQueue():
         message = "not supported Content-Type."
     else:
         try:
+            # add pushed datetime and uuid
+            payload = request.json
+            string_pushed_datetime = datetime.datetime.now().strftime('%Y/%m/%d %H:%M:%S')
+            payload['pushed_datetime'] = string_pushed_datetime
+            payload['uuid'] = str(uuid.uuid4())
+
+            # push queue
             r = redis.Redis(host='redis', port=6379, db=0)
             r.lpush('orderQueue', json.dumps(request.json))
             result = "OK"
         except Exception as e:
-            message = e
+            message = str(e)
 
     if result == "OK":
         response = {"result": result}
